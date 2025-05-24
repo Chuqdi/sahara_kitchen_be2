7214a06ded95563b4b6999cb822be8aed33b15cd
Chuqdi <morganhezekiah111@gmail.com>
2025-05-24 04:08:38 +0100

    g

--- payments/utils/square_client.py
+++ payments/utils/square_client.py
@@ -1,11 +1,11 @@
 # from square.client import Client
-from square import Square
+from square import client
 from django.conf import settings
 import uuid
 
 
 def get_square_client():
-    return Square(
+    return client(
         access_token=settings.SQUARE_ACCESS_TOKEN,
         environment=settings.SQUARE_ENVIRONMENT
     )
